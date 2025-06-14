ead5a06c7d54bcd135982c6ef353035347d77e55
Zhenfang Zhang <fangzhzh@gmail.com>
2025-06-14 17:04:29 +0000

    remove the container's pt-16

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -11,7 +11,7 @@ interface PageContainerProps {
 export default function PageContainer({ children, className, disableScroll = false }: PageContainerProps) {
   return (
     <div className={cn(
-        "container mx-auto px-4 sm:px-6 lg:px-8 pt-16 h-full flex flex-col",
+        "container mx-auto px-4 sm:px-6 lg:px-8 h-full flex flex-col", // Removed pt-16
         disableScroll ? "overflow-y-hidden pb-6" : "overflow-y-auto pb-44", // Conditional overflow and padding
         className
       )}
